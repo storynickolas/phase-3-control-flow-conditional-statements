080e9f185de067546f2ffb36f5976cb9db719495
Nick Story <storynickolasl@gmail.com>
2022-08-30 20:16:37 -0400

    commit tree 5c025cebf557557fb1c5b89e763633772f230322

--- spec/control_flow_spec.rb
+++ spec/control_flow_spec.rb
@@ -19,6 +19,14 @@ describe '#admin_login' do
 
 end
 
+def admin_login(username, password)
+  if (username == 'admin' || username == 'ADMIN') && password == '12345'
+      'Access granted'
+  else
+    'Access denied'
+  end
+end
+
 describe '#hows_the_weather' do
   
   it 'returns "It\'s brisk out there!" if the temperature is below 40' do
@@ -37,6 +45,19 @@ describe '#hows_the_weather' do
     expect(hows_the_weather(77)).to eq("It\'s perfect out there!")
   end
 
+end
+
+def hows_the_weather(temperature)
+  if temperature < 40
+    response = 'brisk'
+  elsif temperature >= 40 && temperature <= 65
+    response = "a little chilly"
+  elsif temperature > 85
+    response = "too dang hot"
+  else 
+    response = "perfect"
+  end
+  "It's #{response} out there!"
 end
 
 describe '#fizzbuzz' do
@@ -62,6 +83,19 @@ describe '#fizzbuzz' do
   end
 
 end
+
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0 
+     "FizzBuzz";
+  elsif num % 3 == 0
+    "Fizz";
+  elsif num % 5 == 0
+    "Buzz";
+  else
+    num;
+  end
+end
+
 
 describe '#calculator' do
   
@@ -89,4 +123,20 @@ describe '#calculator' do
     expect(calculator('nope', 4, 2)).to eq(nil)
   end
   
+end
+
+
+def calculator(operation, num1, num2)
+  case operation
+  when "+"
+    num1 + num2
+  when "-"
+    num1 - num2;
+  when "*"
+    num1 * num2;
+  when "/"
+    num1 / num2;
+  else
+    puts "Invalid operation!"
+  end
 end
